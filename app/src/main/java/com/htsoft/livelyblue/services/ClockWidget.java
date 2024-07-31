6404a1501afb24459c9da607d329d4c3b0f79f8f
Harman-Thind <harmanthind35@gmail.com>
2024-07-30 23:31:16 -0400

    Initial Commit

---
+++ app/src/main/java/com/htsoft/livelyblue/services/ClockWidget.java
@@ -0,0 +1,5 @@
+package com.htsoft.livelyblue.services;
+
+import candybar.lib.services.CandyBarWidgetService;
+
+public class ClockWidget extends CandyBarWidgetService {}
---
+++ app/src/main/java/com/htsoft/livelyblue/services/MuzeiService.java
@@ -0,0 +1,5 @@
+package com.htsoft.livelyblue.services;
+
+import candybar.lib.services.CandyBarMuzeiService;
+
+public class MuzeiService extends CandyBarMuzeiService {}
